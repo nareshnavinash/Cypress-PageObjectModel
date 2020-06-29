2128d8392a612d88d1f2713571866f8b9d924912
Naresh Sekar <naresh@madstreetden.com>
2020-06-29 20:11:31 +0530

    Tags

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,47 +1,52 @@
 /// <reference types="cypress" />
 
+import TestFilter from '../../support/test-filter';
+
+
 context('Actions', () => {
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/actions')
   })
 
   // https://on.cypress.io/interacting-with-elements
+  TestFilter.any(['smoke', 'regression', ''], () => {
+    it('.type() - type into a DOM element', () => {
+      // https://on.cypress.io/type
+      Cypress.currentTest.retries(5)
+      cy.get('.action-email')
+        .type('fake@email.com').should('have.value', 'fake@email.com')
 
-  it('.type() - type into a DOM element', () => {
-    // https://on.cypress.io/type
-    Cypress.currentTest.retries(5)
-    cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+        // .type() with special character sequences
+        .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+        .type('{del}{selectall}{backspace}')
 
-      // .type() with special character sequences
-      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
-      .type('{del}{selectall}{backspace}')
+        // .type() with key modifiers
+        .type('{alt}{option}') //these are equivalent
+        .type('{ctrl}{control}') //these are equivalent
+        .type('{meta}{command}{cmd}') //these are equivalent
+        .type('{shift}')
 
-      // .type() with key modifiers
-      .type('{alt}{option}') //these are equivalent
-      .type('{ctrl}{control}') //these are equivalent
-      .type('{meta}{command}{cmd}') //these are equivalent
-      .type('{shift}')
+        // Delay each keypress by 0.1 sec
+        .type('slow.typing@email.com', { delay: 100 })
+        .should('have.value', 'slow.typing@email.com')
+        .screenshot()
 
-      // Delay each keypress by 0.1 sec
-      .type('slow.typing@email.com', { delay: 100 })
-      .should('have.value', 'slow.typing@email.com')
-      .screenshot()
-
-    cy.get('.action-disabled')
-      // Ignore error checking prior to type
-      // like whether the input is visible or disabled
-      .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
+      cy.get('.action-disabled')
+        // Ignore error checking prior to type
+        // like whether the input is visible or disabled
+        .type('disabled error checking', { force: true })
+        .should('have.value', 'disabled error checking')
+    })
   })
 
-  it('.focus() - focus on a DOM element', () => {
-    // https://on.cypress.io/focus
-    cy.get('.action-focus').focus()
-      .should('have.class', 'focus')
-      .prev().should('have.attr', 'style', 'color: orange;')
+  TestFilter.any(['smoke', ''], () => {
+    it('.focus() - focus on a DOM element', () => {
+      // https://on.cypress.io/focus
+      cy.get('.action-focus').focus()
+        .should('have.class', 'focus')
+        .prev().should('have.attr', 'style', 'color: orange;')
+    })
   })
-
   // it('.blur() - blur off a DOM element', () => {
   //   // https://on.cypress.io/blur
   //   cy.get('.action-blur').type('About to blur').blur()
